c8883060ebb927a179f3994342c5aaa8e7dca104
César <cesarhernandez@campusciff.net>
2016-06-13 22:59:20 +0200

    add

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'prusontchm',
   packages = ['prusontchm'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'mydescription',
   author = 'Cesar Hernandez',
   author_email = 'cesarhernandez@campusciff.net',
   url = 'https://github.com/ciffcesarhernandez/prueba13062016', # use the URL to the github repo
-  download_url = 'https://github.com/ciffcesarhernandez/prueba13062016/tarball/0.2',
+  download_url = 'https://github.com/ciffcesarhernandez/prueba13062016/tarball/0.3',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
