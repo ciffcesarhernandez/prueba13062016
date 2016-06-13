1be4b6c07f50b6b8ec7c6e8c95318318f38a3da0
César <cesarhernandez@campusciff.net>
2016-06-13 20:44:11 +0200

    ahora si

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 
 from distutils.core import setup
 setup(
-  name = 'img',
-  packages = ['img'], # this must be the same as the name above
-  version = '1.0',
-  description = 'my description',
+  name = 'prusontchm',
+  packages = ['prusontchm'], # this must be the same as the name above
+  version = '0.1',
+  description = 'mydescription',
   author = 'Cesar Hernandez',
   author_email = 'cesarhernandez@campusciff.net',
   url = 'https://github.com/ciffcesarhernandez/prueba13062016', # use the URL to the github repo
-  download_url = 'https://github.com/ciffcesarhernandez/prueba13062016/tarball/1.0',
+  download_url = 'https://github.com/ciffcesarhernandez/prueba13062016/tarball/0.1',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
