116014e71814f72f667a7bcbf5dd03b06df2c3a0
César <cesarhernandez@campusciff.net>
2016-06-13 20:27:37 +0200

    commit inicial

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+setup(
+  name = 'img',
+  packages = ['img'], # this must be the same as the name above
+  version = '1.0',
+  description = 'my description',
+  author = 'Cesar Hernandez',
+  author_email = 'cesarhernandez@campusciff.net',
+  url = 'https://github.com/ciffcesarhernandez/prueba13062016', # use the URL to the github repo
+  download_url = 'https://github.com/ciffcesarhernandez/prueba13062016/tarball/1.0',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
